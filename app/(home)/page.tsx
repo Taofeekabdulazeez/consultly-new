e35e80a00b6abb9d3a88827590a30c7a29c87557
ISHOWO ASMAA KHADIJAH <asmaaishowo2912@gmail.com>
2024-05-25 22:47:54 +0100

    landing cont

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
 import ComboBox from "../../components/ComboBox";
+import { BiNotepad } from "react-icons/bi";
+import { AiOutlineIdcard } from "react-icons/ai";
+import { FaCalendarAlt } from "react-icons/fa";
+import { TbCreditCard } from "react-icons/tb";
+import { FaVideo } from "react-icons/fa6";
+import { BiWorld } from "react-icons/bi";
+import { BiMessageDetail } from "react-icons/bi";
 
 export default function Home() {
   return (
@@ -19,7 +26,7 @@ export default function Home() {
             </div>
             <div className="flex flex-1 items-center justify-between">
               <nav className="flex space-x-8  text-sm">
-                <a href="#">Features</a>
+                <a href="#features">Features</a>
                 <a href="#">How It Works</a>
                 <a href="#">Contact</a>
               </nav>
@@ -62,11 +69,184 @@ export default function Home() {
                duration-150 ease-in-out border border-primary/70 hover:bg-purple-300
                 font-medium text-primary px-3 py-2 rounded-full hover:border-gray-300 px-5 md:px-10"
               href="#"
-            >Learn more
+            >
+              Learn more
             </a>
           </div>
         </div>
       </div>
+
+      <section className="scroll-mt-24 py-16 bg-white" id="features">
+        <div className="mx-auto max-w-screen-2xl px-4 text-center sm:px-6 lg:px-8">
+          <div className="space-y-8">
+            <h2 className="text-primary text-base font-medium uppercase tracking-wider">
+              Features
+            </h2>
+            <h3 className="text-balance font-display mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Everything you need in one place
+            </h3>
+            <p className="text-balance header:px-16 mx-auto max-w-4xl px-6 text-slate-700 md:text-lg">
+              Explore our feature set that helps you manage your online
+              consultations workflow.
+            </p>
+          </div>
+          <div>
+            <div className="relative mx-auto mt-8 mx-w-6xl rounded-md bg-gray-100 p-1 sm:p-4">
+              <img src="./dashboard.jpg" alt="dash" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="scroll-mt-24 py-16 bg-white">
+        <div className="mx-auto max-w-4xl text-center sm:px-6 1g:px-8">
+          <div className="grid auto-cols-max auto-rows-fr grid-cols-1 gap-4 px-6 sm:grid-cols-2 sm:gap-8">
+            <div className="flex flex-col p-2 sm:flex-row">
+              <div className="self-start rounded-lg p-3 bg-purple-100">
+                <BiNotepad className="size-[25px] text-indigo-300" />
+              </div>
+              <div className="flex flex-col text-left sm:ml-6">
+                <div className="mt-4 flex-1 space-y-2 sm:mt-0">
+                  <h3 className="font-bold">Scheduling form</h3>
+                  <p>
+                    Complete the simple form to send your client an invitation
+                    by email.
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="text-primary mt-2 cursor-pointer underline"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col p-2 sm:flex-row">
+              <div className="self-start rounded-lg p-3 bg-blue-50">
+                <AiOutlineIdcard className="size-[25px] text-blue-300" />
+              </div>
+              <div className="flex flex-col text-left sm:ml-6">
+                <div className="mt-4 flex-1 space-y-2 sm:mt-0">
+                  <h3 className="font-bold">Public profile</h3>
+                  <p>
+                    Create your Consultly profile and share it anywhere on the
+                    web.
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="text-primary mt-2 cursor-pointer underline"
+                >
+                  See the demo
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col p-2 sm:flex-row">
+              <div className="self-start rounded-lg p-3 bg-pink-100">
+                <FaCalendarAlt className="size-[25px] text-pink-400" />
+              </div>
+              <div className="flex flex-col text-left sm:ml-6">
+                <div className="mt-4 flex-1 space-y-2 sm:mt-0">
+                  <h3 className="font-bold">Availability calendar</h3>
+                  <p>
+                    Share the calendar with available time slots to automate
+                    scheduling meetings and accepting payments.
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="text-primary mt-2 cursor-pointer underline"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col p-2 sm:flex-row">
+              <div className="self-start rounded-lg p-3 bg-yellow-50">
+                <TbCreditCard className="size-[25px] text-orange-400" />
+              </div>
+              <div className="flex flex-col text-left sm:ml-6">
+                <div className="mt-4 flex-1 space-y-2 sm:mt-0">
+                  <h3 className="font-bold">Online payment solution</h3>
+                  <p>
+                    Accept payments from your clients in easy, fast and secure
+                    way.
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="text-primary mt-2 cursor-pointer underline"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col p-2 sm:flex-row">
+              <div className="self-start rounded-lg p-3 bg-indigo-100">
+                <FaVideo className="size-[30px] text-purple-300" />
+              </div>
+              <div className="flex flex-col text-left sm:ml-6">
+                <div className="mt-4 flex-1 space-y-2 sm:mt-0">
+                  <h3 className="font-bold">Video conferencing platform</h3>
+                  <p>
+                    Use our browser built-in video solution to connect with
+                    clients without the need for external applications.
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="text-primary mt-2 cursor-pointer underline"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col p-2 sm:flex-row">
+              <div className="self-start rounded-lg p-3 bg-indigo-200">
+                <BiWorld className="size-[25px] text-blue-400" />
+              </div>
+              <div className="flex flex-col text-left sm:ml-6">
+                <div className="mt-4 flex-1 space-y-2 sm:mt-0">
+                  <h3 className="font-bold">Time zone support</h3>
+                  <p>Always be on time with our time zone converter.</p>
+                </div>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="text-primary mt-2 cursor-pointer underline"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col p-2 sm:flex-row">
+              <div className="self-start rounded-lg p-3 bg-pink-50">
+                <BiMessageDetail className="size-[25px] text-pink-400" />
+              </div>
+              <div className="flex flex-col text-left sm:ml-6">
+                <div className="mt-4 flex-1 space-y-2 sm:mt-0">
+                  <h3 className="font-bold">Meeting requests</h3>
+                  <p>
+                    No more chaos in your social media inboxes. Receive meeting
+                    requests in one place.
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="text-primary mt-2 cursor-pointer underline"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </body>
   );
 }
