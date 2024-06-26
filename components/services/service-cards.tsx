24bca26c27cf6d7a4fe7a87cc340ca46ec27b339
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-06-26 15:01:20 +0100

    Update service-cards.tsx

--- components/services/service-cards.tsx
+++ components/services/service-cards.tsx
@@ -5,12 +5,11 @@ import { useServices } from "./services-context";
 
 export default function ServiceCards() {
   const { services } = useServices();
-  const colors = ["orange", "blue", "red", "purple", "indigo", "green"];
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
       {services.map((service, i) => (
-        <Service serviceId={service.id} key={service.id} color={colors[i]} />
+        <Service serviceId={service.id} key={service.id} />
       ))}
     </div>
   );
