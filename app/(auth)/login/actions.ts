4d37b0a70478c32345555e53a98f09300eed9bd8
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-06-27 19:34:48 +0100

    Update actions.ts

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -16,6 +16,7 @@ export async function login(formData: FormData) {
   const { error } = await supabase.auth.signInWithPassword(signInData);
 
   if (error) {
+    console.log(error);
     return redirect("/error");
   }
 
