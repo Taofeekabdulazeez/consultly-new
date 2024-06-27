646e7d90acd478529cb631e443461a8e04d18873
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-06-27 16:57:49 +0100

    Update profile-photo.tsx

--- components/profile/profile-photo.tsx
+++ components/profile/profile-photo.tsx
@@ -1,15 +1,14 @@
 import profilePlaceholder from "@/public/profile-placeholder.png";
 import Image from "next/image";
 import { P } from "../ui/typography";
-import { auth } from "@/lib/auth";
+// import { auth } from "@/lib/auth";
 
 export default async function ProfilePhoto() {
-  const session = await auth();
+  // const session = await auth();
   return (
     <div className="flex gap-10 items-center mb-10">
       <div className="relative h-[6rem] aspect-square">
-        <img
-          // fill
+        {/* <img
           className="absolute rounded-full"
           src={
             session?.user?.image
@@ -17,7 +16,7 @@ export default async function ProfilePhoto() {
               : "./profile-placeholder.png"
           }
           alt="profile picture"
-        />
+        /> */}
       </div>
       <div>
         <label className="focus:ring-primary relative inline-flex cursor-pointer items-center justify-center rounded-md border border-gray-300 bg-gray-50 px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2">
